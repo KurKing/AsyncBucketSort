54fdbc5eab71f2477d33a7f26b0ef088bacd4398
Kurkin Oleksii <oleksii.kurkin@airslate.com>
2023-05-09 12:33:05 +0300

    - add time counting

--- BucketSort/main.swift
+++ BucketSort/main.swift
@@ -7,4 +7,46 @@
 
 import Foundation
 
-print("Hello, World!")
+// Before 2800 async array
+// On 2800 async array becomes quicker than sync in 1.1 times
+// On 10000 in 1.9 times
+// On 100000 in 2.4 times
+// On 1000000 in 2.65 times
+
+let arrayToSort = (0...100000).compactMap({ (0...$0).randomElement() }).shuffled()
+let semaphore = DispatchSemaphore(value: 0)
+
+var sorted: [Int]!
+var syncDateDiff = 0.0
+
+DispatchQueue.global().async {
+    
+    let syncDate = Date().timeIntervalSince1970
+    sorted = arrayToSort.bucketSort()
+    syncDateDiff = Date().timeIntervalSince1970 - syncDate
+    
+    print("Sync time: \(syncDateDiff.string)")
+    print("Is correct: \(sorted == arrayToSort.sorted()) \n")
+    
+    semaphore.signal()
+}
+
+semaphore.wait()
+
+let asyncDate = Date().timeIntervalSince1970
+arrayToSort.bucketSort(completion: { result in
+
+    let asyncDateDiff = Date().timeIntervalSince1970 - asyncDate
+    
+    print("Async time: \(asyncDateDiff.string)")
+    print("Is correct: \(sorted == result)")
+    
+    if asyncDateDiff < syncDateDiff {
+        
+        print("\nAsync quicker in \((syncDateDiff / asyncDateDiff).string) times\n")
+    }
+    
+    semaphore.signal()
+})
+
+semaphore.wait()
